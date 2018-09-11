dcb0375da568529ac2ac0b7dc2f70986ec3c54bd
Mateusz Then <Mateusz@MacBook-Air-Mateusz.local>
2018-09-11 09:38:52 +0200

    19.1

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -25,7 +25,7 @@ public class SimpleEmailServiceTest {
     @Test
     public void shouldSendEmail() {
         //Given
-        Mail mail = new Mail("test@test.com", "Test", "Test message", "testCc@test.com");
+        Mail mail = new Mail("test@test.com", "Test", "Test message"); //, "testCc@test.com");
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
